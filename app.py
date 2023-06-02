0786e41c4f89193bd08f58cf0f7a4b2c07991eec
mylene marsden <mylenemarsden84@gmail.com>
2023-06-02 23:30:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -208,8 +208,6 @@ app.layout = html.Div(
 @app.callback(
     Output("price-chart-figure", "figure"), 
     Output("scatter-plot-figure", "figure"),
-    # Output("top-ten-chart-figure", "figure"),
-    # Output("top-ten-table-figure", "figure"),
 
     Input("country-filter", "value"),
  
@@ -230,8 +228,6 @@ def update_charts(country):
 
     word_cloud_items= generate_wordcloud_data(filtered_data)
 
-    print(filtered_data)
-
     summary = filtered_data.groupby('Analysis').agg({'Analysis':'size', 'Polarity':'mean'}).rename(columns={'Analysis':'count','mean':'mean_sent'}).reset_index()
 
     fig_bar_line = go.Figure(
@@ -245,136 +241,8 @@ def update_charts(country):
 
     fig_scatter_plot = px.scatter(filtered_data, x="Polarity", y="Subjectivity", color="Analysis",
                  size='Subjectivity', hover_data=['Polarity'])
-    
-    #fig_bar_line = px.bar(analysis, x='Analysis', y='count')
-    # # print(filtered_data)
-    # text = " ".join(review for review in filtered_data.description)
 
-    # job_cloud= filtered_data.groupby(["description"])["description"].count().reset_index(name="count")
-    # print("job_cloud")
-    # print(job_cloud)
-    # print(newGraph)
-    # stopwords = set(STOPWORDS)
-    # stopwords.update(["drink", "now", "wine", "flavor", "flavors"])
-    # wordcloud = WordCloud(stopwords=stopwords,height=500, width=500, background_color="white", contour_color='white', colormap="magma").generate(newGraph)
-    # buf = io.BytesIO() # in-memory files
-    # plt.figure()
-    # plt.imshow(wordcloud, interpolation="bilinear")
-    # plt.axis("off")
-    # plt.savefig(buf, format = "png", dpi=600, bbox_inches = 'tight', pad_inches = 0) # save to the above file object
-    # data = base64.b64encode(buf.getbuffer()).decode("utf8") # encode to html elements
-    # plt.close()
-    # fig_pie_chart = px.pie(filtered_data, values='salary_in_usd', names='work_type', title='Salary per Work Year', color_discrete_sequence=px.colors.sequential.RdBu_r)
-
-    # #eab676
-    # remote_ratio_plot = go.Figure()
-    # remote_ratio_plot.add_trace(go.Box(y=filtered_data['salary_in_usd'][(filtered_data['remote_ratio'] == 0)],  fillcolor="#042f66",marker=dict(color='#042f66'), quartilemethod="linear", name="No Remote Work (<20%)" ))
-    # remote_ratio_plot.add_trace(go.Box(y=filtered_data['salary_in_usd'][(filtered_data['remote_ratio'] == 50)], fillcolor="#eab676",marker=dict(color='#eab676'), quartilemethod="inclusive", name="Partially remote (~50%)"))
-    # remote_ratio_plot.add_trace(go.Box(y=filtered_data['salary_in_usd'][(filtered_data['remote_ratio'] == 100)], fillcolor="#e28743",marker=dict(color='#e28743'), quartilemethod="exclusive", name="Fully remote (>80%)"))
-    # remote_ratio_plot.update_traces(boxpoints='all', jitter=0)
-    # remote_ratio_plot.update_layout(title=dict(text="Media Salary per Work Type"))
-
-    # summary = filtered_data.groupby('job_title').agg({'job_title':'size', 'salary_in_usd':'mean'}).rename(columns={'job_title':'count','mean':'mean_sent'}).reset_index()
-    # sanitized_data=summary.sort_values(by=['salary_in_usd'],  ascending=False).head(10)
-
-    # count=sanitized_data["count"].values
-    # job_title=sanitized_data["job_title"].values
-    # salary_in_usd=sanitized_data["salary_in_usd"].values
-
-    # sanitized_data.rename(columns = {'count':'Count', 'job_title':'Job Description','salary_in_usd':'Salary'}, inplace = True)
-    # fig_table_data=ff.create_table(sanitized_data.round(2), height_constant=60, colorscale=px.colors.sequential.Blues_r,)
-
-    # fig_bar_line = go.Figure(
-    # data=go.Bar(
-    #     x=job_title,
-    #     y=count,
-    #     name="Count of Jobs",
-    #     marker=dict(color="#1e81b0"),
-    #     )
-    # )
-
-    # fig_bar_line.add_trace(
-    # go.Scatter(
-    #     x=job_title,
-    #     y=salary_in_usd,
-    #     yaxis="y2",
-    #     name="Average Salary",
-    #     marker=dict(color="crimson"),
-    #     )
-    # )
-
-    # fig_bar_line.update_layout(
-    # legend=dict(orientation="v"),
-    # yaxis=dict(
-    #     title=dict(text="Count of Jobs"),
-    #     side="left",
-    # ),
-    # yaxis2=dict(
-    #     title=dict(text="Average Salary"),
-    #     side="right",
-    #     overlaying="y",
-    #     tickmode="sync",
-    #     ),
-    #     title=dict(text="Top 10 Jobs")
-    # )
-    # fig_pie_chart=[]
-    # remote_ratio_plot=[],
-    # fig_bar_line=[],
-    # fig_table_data=[]
-    # return fig_pie_chart,remote_ratio_plot,fig_bar_line,fig_table_data
-    # text = " ".join(review for review in filtered_data.description)
-    # # Create stopword list:
-    # stopwords = set(STOPWORDS)
-    # stopwords.update(["drink", "now", "wine", "flavor", "flavors"])
-
-    # Generate a word cloud image
-    # wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
-
-    # Display the generated image:
-    # the matplotlib way:
-    # plt.imshow(wordcloud, interpolation='bilinear')
-    # fig_pie_chart=plt.axis("off")
-
-    # newGraph =  [value[0]:value[1]]
-    #print(newGraph)
-    # wordcloud = WordCloud(height=500, width=500, background_color="white", contour_color='white', colormap="magma").generate(text)
-    # buf = io.BytesIO() # in-memory files
-    # plt.figure()
-    # plt.imshow(wordcloud, interpolation="bilinear")
-    # plt.axis("off")
-    # plt.savefig(buf, format = "png", dpi=600, bbox_inches = 'tight', pad_inches = 0) # save to the above file object
-    # data_ = base64.b64encode(buf.getbuffer()).decode("utf8") # encode to html elements
-    # plt.close()
-    
-    # stopwords = set(STOPWORDS)
-    # stopwords.update(["drink", "now", "wine", "flavor", "flavors"])
-
-    # # Generate a word cloud image
-    # wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
-    # buf = io.BytesIO() # in-memory files
-    # # Display the generated image:
-    # # the matplotlib way:
-    # plt.imshow(wordcloud, interpolation='bilinear')
-    # plt.axis("off")
-    # plt.savefig(buf, format = "png", dpi=600, bbox_inches = 'tight', pad_inches = 0) # save to the above file object
-    # data_ = base64.b64encode(buf.getbuffer()).decode("utf8") # encode to html elements
-    # plt.close()
-
-    # security_data=text
-    #return "data:image/png;base64,{}".format(data_)
     return fig_bar_line, fig_scatter_plot
-
-# def plot_wordcloud(data):
-#     d = {a: x for a, x in data.values}
-#     wc = WordCloud(background_color='black', width=480, height=360)
-#     wc.fit_words(d)
-#     return wc.to_image()
-
-# @app.callback(dd.Output('image_wc', 'src'), [dd.Input('image_wc', 'id')])
-# def make_image(b):
-#     img = BytesIO()
-#     plot_wordcloud(data=dfm).save(img, format='PNG')
-#     return 'data:image/png;base64,{}'.format(base64.b64encode(img.getvalue()).decode())
 
 
 
